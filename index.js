2231b04e15c5a62c8a2fcca5e06a89069e67d96d
zacanger <zac@zacanger.com>
2017-10-29 13:42:57 -0600

    Clean up a bit.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-'use strict'
-
-const { STATUS_CODES } = require('http')
-const arg = process.argv[2]
-const { log } = console
+const statusCodes = require('http').STATUS_CODES
+const log = console.log
 const help = () => log(`
   please provide a valid http status code, or all, or full string
   usage example:
@@ -13,23 +10,27 @@ const help = () => log(`
   httpcodes 'Moved Permanently'
 `)
 
-if (!arg) {
+const main = (arg) => {
+  if (!arg) {
+    return help()
+  }
+
+  if (arg === 'all') {
+    return log(JSON.stringify(statusCodes, null, 2))
+  }
+
+  if (statusCodes[arg]) {
+    return log(`${arg}: ${statusCodes[arg]}`)
+  }
+
+  const getKeyByVal = (object, value) =>
+    Object.keys(object).find((key) => object[key].toLowerCase() === value.toLowerCase())
+
+  if (getKeyByVal(statusCodes, arg)) {
+    return log(getKeyByVal(statusCodes, arg))
+  }
+
   return help()
 }
 
-if (arg === 'all') {
-  return log(JSON.stringify(STATUS_CODES, null, 2))
-}
-
-if (STATUS_CODES[arg]) {
-  return log(`${arg}: ${STATUS_CODES[arg]}`)
-}
-
-const getkeybyvalue = (object, value) =>
-  Object.keys(object).find((key) => object[key].toLowerCase() === value.toLowerCase())
-
-if (getkeybyvalue(STATUS_CODES, arg)) {
-  return log (getkeybyvalue(STATUS_CODES, arg))
-}
-
-return help()
+main(process.argv[2])
